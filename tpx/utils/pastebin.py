bd15e2428100e94c338a6585d8d6df9ee9272b35
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <DevUnnati86@gmail.com>
2025-08-24 09:54:01 +0530

    Update pastebin.py

--- tpx/utils/pastebin.py
+++ tpx/utils/pastebin.py
@@ -13,7 +13,7 @@ async def post(url: str, *args, **kwargs):
         return data
 
 
-async def LoyBin(text):
+async def toxicBin(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
         return
