a341fc13aa70eb66a459b48f690dc3b23c087586
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <DevUnnati86@gmail.com>
2025-08-24 10:08:21 +0530

    Update reload.py

--- tpx/plugins/tools/reload.py
+++ tpx/plugins/tools/reload.py
@@ -9,7 +9,7 @@ from tpx import app
 from tpx.core.call import toxic
 from tpx.misc import db
 from tpx.utils.database import get_assistant, get_authuser_names, get_cmode
-from toxic.utils.decorators import ActualAdminCB, AdminActual, language
+from tpx.utils.decorators import ActualAdminCB, AdminActual, language
 from tpx.utils.formatters import alpha_to_int, get_readable_time
 from config import BANNED_USERS, adminlist, lyrical
 
@@ -123,3 +123,4 @@ async def stop_download(client, CallbackQuery: CallbackQuery, _):
         except:
             return await CallbackQuery.answer(_["tg_8"], show_alert=True)
     await CallbackQuery.answer(_["tg_9"], show_alert=True)
+
