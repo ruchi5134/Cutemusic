54953089d8920451a9ba18822225a266a0467f05
ruchi5134 <kn043613@gmail.com>
2025-08-24 14:20:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "",
+    "https://github.com/ruchi5134/Cutemusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
